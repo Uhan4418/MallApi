f71d5fc9358468a0e34870be485a9ec5810b5a73
Uhan4418 <515235337@qq.com>
2020-04-01 20:49:55 +0800

    添加图片上传接口

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express')
+//  用来解析post请求的数据
 const bodyParser = require('body-parser')
 const path = require('path')
 // const tokenMiddleWare = require('./middleware/tokenMiddleWare')
 const app = express()
 
+//解决跨域跨域
+const cors = require('cors');
+//通过第三方中间件cors来实现跨域的问题
+app.use(cors());
+
 // 引入数据库连接文件 启动服务器的时候同时启动数据库
 const db = require('./db/connect')
 
-//post 数据的解析 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// 解析post数据
+// 解析表单格式的数据 x-www-form-urlencode
+app.use(bodyParser.urlencoded({extended: false}));
+// 解析json数据格式
+app.use(bodyParser.json());
 
 //静态资源路径
-app.use('/public',express.static(path.join(__dirname,'./public')))
+app.use('/public',express.static(path.join(__dirname,'./public/img')))
 
 /*
   路由
@@ -23,16 +31,19 @@ app.use('/public',express.static(path.join(__dirname,'./public')))
 
 let  userRouter = require("./routers/userRouter")
 app.use('/admin/user',userRouter) //登录注册不需要加锁
+
 let  goodRouter = require("./routers/goodRouter")
 app.use('/admin/goods',goodRouter) 
+
 let  orderRouter = require("./routers/orderRouter")
 app.use('/admin/orders',orderRouter) 
+
 let  uploadRouter = require("./routers/uploadRouter")
-app.use('/admin/uploads',uploadRouter)
-// let  uploadRouter = require('./router/uploadRouter')
+app.use('/admin/upload',uploadRouter)
+
 // let  foodRouter = require('./router/foodRouter')
 // app.use('/admin/food',tokenMiddleWare,foodRouter) //需要加锁的
-// app.use('/admin/upload',uploadRouter)
+
 app.listen(3000,()=>{
 	console.log("server start!")
   console.log(`/**
